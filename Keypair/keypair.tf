b01bcd59203f6578041154e85b55591e9bc726af
ephillipamoo <eamoophillip@outlook.com>
2025-04-22 15:37:52 +0100

    adding aws datasource code

--- Keypair/keypair.tf
+++ Keypair/keypair.tf
@@ -7,3 +7,9 @@ output "keypair" {
   value = data.aws_key_pair.keypair
 }
 
+resource "aws_instance" "datasrc_instance" {
+  ami = var.instance
+  intsance_type = "t2_micro"
+  key_name = data.aws_key_pair.keypair.key_name
+  
+}
